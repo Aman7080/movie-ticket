a35735ca85c8dcaad889216e86475ad2914088d0
Aman Kumar <kumar7080aman7080@gmail.com>
2023-05-24 19:47:02 +0530

    api connected

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,65 +15,116 @@ function App() {
     D2: 0,
   });
 
+  const [bookingData, setBookingData] = useState({
+    movie: "",
+    slot: "",
+    seats: {
+      A1: 0,
+      A2: 0,
+      A3: 0,
+      A4: 0,
+      D1: 0,
+      D2: 0,
+    },
+  });
+
   useEffect(() => {
-    const movies = JSON.parse(localStorage.getItem("movie"));
+    const movies = JSON.parse(localStorage.getItem("movie") as string);
     if (movies) {
       setSelectedMovie(movies);
     }
-    const slot = JSON.parse(localStorage.getItem("slot"));
+    const slot = JSON.parse(localStorage.getItem("slot") as string);
     if (slot) {
       setSelectedSlot(slot);
     }
-    const seat = JSON.parse(localStorage.getItem("seat"));
+    const seat = JSON.parse(localStorage.getItem("seat") as string);
     if (seat) {
       setSelectedSeat(seat);
     }
 
-    const totalseat = JSON.parse(localStorage.getItem("totalseat"));
+    const totalseat = JSON.parse(localStorage.getItem("totalseat") as string);
     if (totalseat) {
       setTotalSeat(totalseat);
     }
+  }, [selectedMovie]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/booking")
+      .then((response) => response.json())
+      .then((data) => {
+        // Extract the required data from the API response
+        const { movie, slot, seats } = data;
+        setBookingData({ movie, slot, seats });
+      });
   }, []);
 
   const handleMovieClick = (name: string | SetStateAction<string>) => {
     setSelectedMovie(name === selectedMovie ? "" : name);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("movie", JSON.stringify(selectedMovie));
-  },[selectedMovie])
-
+  }, [selectedMovie]);
 
   const handleSlotClick = (slot: SetStateAction<string>) => {
     setSelectedSlot(slot === selectedSlot ? "" : slot);
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("slot", JSON.stringify(selectedSlot));
-  },[selectedSlot])
+  }, [selectedSlot]);
 
   const handleSeatClick = (seat: SetStateAction<string>) => {
     setSelectedSeat(seat === selectedSeat ? "" : seat);
   };
-
 
   const handleSeatChange = (seat: string, value: number) => {
     setTotalSeat((prevTotalSeat) => ({
       ...prevTotalSeat,
       [seat]: value,
     }));
-   
   };
 
-  useEffect(()=>{
-    localStorage.setItem("totalseat",JSON.stringify(totalSeat))
-  },[totalSeat])
-
+  useEffect(() => {
+    localStorage.setItem("totalseat", JSON.stringify(totalSeat));
+  }, [totalSeat]);
 
   const bookMovie = () => {
     console.log("Movie = " + selectedMovie);
     console.log("Slot = " + selectedSlot);
     console.log("total Seat = " + totalSeat);
+    fetch("http://localhost:8080/api/booking", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        movie: selectedMovie,
+        slot: selectedSlot,
+        seats: totalSeat,
+      }),
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("Booking Created Successfully");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    setSelectedMovie("");
+    setSelectedSlot("");
+    setTotalSeat({
+      A1: 0,
+      A2: 0,
+      A3: 0,
+      A4: 0,
+      D1: 0,
+      D2: 0,
+    });
+    localStorage.clear();
   };
 
   return (
@@ -125,7 +176,9 @@ function App() {
               <input
                 type="number"
                 id={`seat-${seat}`}
-                onChange={(e) => handleSeatChange(seat, parseInt(e.target.value))}
+                onChange={(e) =>
+                  handleSeatChange(seat, parseInt(e.target.value))
+                }
                 className="input"
               />
             </div>
@@ -134,7 +187,49 @@ function App() {
         <button onClick={bookMovie}>Book Now</button>
       </div>
       <div className="last-order">
-        <h2>Last Bookings Details : </h2>
+        {bookingData.movie ? (
+          <>
+            <h2>Last Bookings Details : </h2>
+            <p>
+              <strong>seats</strong>
+            </p>
+
+            <p>
+              <strong>A1:</strong>
+              {bookingData?.seats.A1}
+            </p>
+            <p>
+              <strong>A2:</strong>
+              {bookingData?.seats.A2}
+            </p>
+            <p>
+              <strong>A3:</strong>
+              {bookingData?.seats.A3}
+            </p>
+            <p>
+              <strong>A4:</strong>
+              {bookingData?.seats.A4}
+            </p>
+            <p>
+              <strong>D1:</strong>
+              {bookingData?.seats.D1}
+            </p>
+            <p>
+              <strong>D2:</strong>
+              {bookingData?.seats.D2}
+            </p>
+            <p>
+              <strong>slot:</strong>
+              {bookingData?.slot}
+            </p>
+            <p>
+              <strong>movie:</strong>
+              {bookingData?.movie}
+            </p>
+          </>
+        ) : (
+          <h2>No previous booking found</h2>
+        )}
       </div>
     </div>
   );
